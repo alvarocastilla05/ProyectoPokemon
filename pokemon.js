9f51193baaed107eb9c286c75f9717a87b2d29a8
Lucas Falla Urtiaga <falla.urluc24@triana.salesianos.edu>
2024-10-09 11:36:29 +0200

    Colores a las tarjetas pokémon incorporados

--- pokemon.js
+++ pokemon.js
@@ -12,90 +12,12 @@ $(document).ready(function () {
 
             let pokemonId = pokemon.url.split("/")[6];
 
-            let tipo1 = pokemon.url.forms.name;
-
-            switch(tipo1){
-              case "normal":
-                  $(".tipoDominante").attr("style", "background-color: rgba(170,166,127,.4)");
-                  break;
-
-              case "fighting":
-                  $(".tipoDominante").attr("style", "background-color: #C12239");
-                  break;
-
-              case "flying":
-                  $(".tipoDominante").attr("style", "background-color: #A891EC");
-                  break;
-
-              case "ground":
-                  $(".tipoDominante").attr("style", "background-color: #DEC16B");
-                  break;
-
-              case "poison":
-                  $(".tipoDominante").attr("style", "background-color: #A43E9E");
-                  break;
-              
-              case "rock":
-                  $(".tipoDominante").attr("style", "background-color: #B69E31");
-                  break; 
-
-              case "bug":
-                  $(".tipoDominante").attr("style", "background-color: #A7B723");
-                  break; 
-
-              case "ghost":
-                  $(".tipoDominante").attr("style", "background-color: #70559B");
-                  break; 
-
-              case "steel":
-                  $(".tipoDominante").attr("style", "background-color: #B7B9D0");
-                  break; 
-
-              case "fire":
-                  $(".tipoDominante").attr("style", "background-color: #F57D31");
-                  break;
-              
-              case "water":
-                  $(".tipoDominante").attr("style", "background-color: #6493EB");
-                  break;
-
-              case "grass":
-                  $(".tipoDominante").attr("style", "background-color: rgba(116,203,72,.4)");
-                  break;
-
-              case "electric":
-                  $(".tipoDominante").attr("style", "background-color: #F9CF30");
-                  break;
-
-              case "psychic":
-                  $(".tipoDominante").attr("style", "background-color: #FB5584");
-                  break;
-
-              case "ice":
-                  $(".tipoDominante").attr("style", "background-color: #9AD6DF");
-                  break;
-
-              case "dragon":
-                  $(".tipoDominante").attr("style", "background-color: #7037FF");
-                  break;
-
-              case "dark":
-                  $(".tipoDominante").attr("style", "background-color: #75574C");
-                  break;
-
-              case "fairy":
-                  $(".tipoDominante").attr("style", "background-color: #E69EAC");
-                  break;
-              
-              default:
-                  $(".tipoDominante").attr("style", "background-color: rgba(116,203,72,.4)");
-          }
-
+            cambiarPaletaColores(pokemonId);
 
             let template = `<div class="card border-0 jockey-one-regular mx-3 my-4" style="width: 290px;">
         
                 <!--Parte superior-->
-                <div class="tipoDominante border-5 rounded-4 bordeCaja sombreado">
+                <div class="tipoDominante${pokemonId} border-5 rounded-4 bordeCaja sombreado">
                     <a href="./detalles.html?pid=${pokemonId}"><img src="https://raw.githubusercontent.com/PokeAPI/sprites/refs/heads/master/sprites/pokemon/${pokemonId}.png" class="card-img-top rounded-4" alt="imgPokemon" id="imgCard"></a>
                 </div>
 
@@ -119,6 +41,93 @@ $(document).ready(function () {
 
             </div>`;
             $("#data-content").append(template);
+
+            function cambiarPaletaColores(pokemonId){
+                $.ajax({
+                    url: `https://pokeapi.co/api/v2/pokemon/${pokemonId}`,
+                    method: "GET",
+                  }).done(function (resp) {
+                    let tipo1 = resp.types[0].type.name;
+
+                    switch(tipo1){
+                        case "normal":
+                            $(`.tipoDominante${pokemonId}`).attr("style", "background-color: rgba(170,166,127,.4)");
+                            break;
+          
+                        case "fighting":
+                            $(`.tipoDominante${pokemonId}`).attr("style", "background-color: #C12239");
+                            break;
+          
+                        case "flying":
+                            $(`.tipoDominante${pokemonId}`).attr("style", "background-color: #A891EC");
+                            break;
+          
+                        case "ground":
+                            $(`.tipoDominante${pokemonId}`).attr("style", "background-color: #DEC16B");
+                            break;
+          
+                        case "poison":
+                            $(`.tipoDominante${pokemonId}`).attr("style", "background-color: #A43E9E");
+                            break;
+                        
+                        case "rock":
+                            $(`.tipoDominante${pokemonId}`).attr("style", "background-color: #B69E31");
+                            break; 
+          
+                        case "bug":
+                            $(`.tipoDominante${pokemonId}`).attr("style", "background-color: #A7B723");
+                            break; 
+          
+                        case "ghost":
+                            $(`.tipoDominante${pokemonId}`).attr("style", "background-color: #70559B");
+                            break; 
+          
+                        case "steel":
+                            $(`.tipoDominante${pokemonId}`).attr("style", "background-color: #B7B9D0");
+                            break; 
+          
+                        case "fire":
+                            $(`.tipoDominante${pokemonId}`).attr("style", "background-color: #F57D31");
+                            break;
+                        
+                        case "water":
+                            $(`.tipoDominante${pokemonId}`).attr("style", "background-color: #6493EB");
+                            break;
+          
+                        case "grass":
+                            $(`.tipoDominante${pokemonId}`).attr("style", "background-color: rgba(116,203,72,.4)");
+                            break;
+          
+                        case "electric":
+                            $(`.tipoDominante${pokemonId}`).attr("style", "background-color: #F9CF30");
+                            break;
+          
+                        case "psychic":
+                            $(`.tipoDominante${pokemonId}`).attr("style", "background-color: #FB5584");
+                            break;
+          
+                        case "ice":
+                            $(`.tipoDominante${pokemonId}`).attr("style", "background-color: #9AD6DF");
+                            break;
+          
+                        case "dragon":
+                            $(`.tipoDominante${pokemonId}`).attr("style", "background-color: #7037FF");
+                            break;
+          
+                        case "dark":
+                            $(`.tipoDominante${pokemonId}`).attr("style", "background-color: #75574C");
+                            break;
+          
+                        case "fairy":
+                            $(`.tipoDominante${pokemonId}`).attr("style", "background-color: #E69EAC");
+                            break;
+                        
+                        default:
+                            $(`.tipoDominante${pokemonId}`).attr("style", "background-color: rgba(116,203,72,.4)");
+                    }
+
+                  })
+            } 
         });
       });
     }
